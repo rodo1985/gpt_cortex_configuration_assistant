5b06e9a492943cfd9f7cce16f5386f1d5bfce03d
rodo1985 <rodo1985@gmail.com>
2023-04-30 20:02:48 +0200

    Working first Cortex example

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import os
 import yaml
-from langchain.llms import OpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.embeddings.cohere import CohereEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.elastic_vector_search import ElasticVectorSearch
-from langchain.vectorstores import Chroma
-from langchain.docstore.document import Document
-from langchain.prompts import PromptTemplate
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.vectorstores import FAISS 
+from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.output_parsers.regex import RegexParser
 
 def load_config():
     with open('app_config.yaml', 'r') as file:
@@ -23,50 +20,65 @@ def main():
 
     load_config()
 
-    # with open('docs/App.config.xml') as f:
-    #     state_of_the_union = f.read()
-    # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    # texts = text_splitter.split_text(state_of_the_union)
-
-    # embeddings = OpenAIEmbeddings()
-    # docsearch = Chroma.from_texts(texts, embeddings, metadatas=[{"source": str(i)} for i in range(len(texts))])
-    # query = "¿Como podria configurar la pelicula del sensor para utilizar 100 imagenes?"
-    # docs = docsearch.similarity_search(query)
-    
     docs_path = 'docs'
     
     # load all documents in docs folder
     doc_list = os.listdir(docs_path)
 
-    from langchain.document_loaders import TextLoader
-    from langchain.indexes import VectorstoreIndexCreator
-    
-    loaders = []
+    texts = None
+
     for doc_file in doc_list:
-        loader = TextLoader(os.path.join(docs_path, doc_file), encoding='utf8')
-        loaders.append(loader)
+        path = os.path.join(docs_path, doc_file)
 
-    index = VectorstoreIndexCreator().from_loaders(loaders)
+        with open(path) as f:
+            state_of_the_union = f.read()
 
-    query = "¿Como agregar mas imagenes a la pelicula del sensor?"
-    print(index.query(query))
- 
-    # template = """Eres un asistente automatico de un software llamado Cortex de la empresa bcnvision. Tu objetivo es poder responder preguntas tecnicas sobre la configuración de este software.
-    # Para cualquier pregunta que no sea relacionada con Cortex deberas contestar que no lo sabes o que eres un asistente especializado en Cortex y que no puedes contestar nada que no este relacionado con el software. 
-    # Si no sabes responder alguna pregunta indica al usario que se ponga en contacto con el departamente de Backoffice.
-
-    # PREGUNTA: {question}
-    # =========
-    # {summaries}
-    # =========
-    # """
-
-    # PROMPT = PromptTemplate(template=template, input_variables=["summaries", "question"])
-
-    # chain = load_qa_with_sources_chain(OpenAI(temperature=0), chain_type="stuff", prompt=PROMPT)
+        # Splitting up the text into smaller chunks for indexing
+        text_splitter = CharacterTextSplitter(        
+            separator = "\n",
+            chunk_size = 1000,
+            chunk_overlap  = 200, #striding over the text
+            length_function = len,
+        )
+        if texts is None:
+            texts = text_splitter.split_text(state_of_the_union)
+        else:
+            texts += text_splitter.split_text(state_of_the_union)
+        break
     
-    # query = "¿Como agreagar mas imagenes a la pelicula del sensor?"
-    # print(chain({"input_documents": loaders, "question": query}, return_only_outputs=True))
+    # Download embeddings from OpenAI
+    embeddings = OpenAIEmbeddings()
+    docsearch = FAISS.from_texts(texts, embeddings)
+    print(docsearch.embedding_function)
 
+    prompt_template = """Eres un asistente automatico de un software llamado Cortex de la empresa bcnvision. Tu objetivo es poder responder preguntas tecnicas sobre la configuración de este software.
+    Para cualquier pregunta que no sea relacionada con Cortex deberas contestar que no lo sabes o que eres un asistente especializado en Cortex y que no puedes contestar nada que no este relacionado con el software. 
+    Si no sabes responder alguna pregunta indica al usario que se ponga en contacto con el departamente de Backoffice.
+
+    # Contexto:
+    # ---------
+    # {context}
+    # ---------
+    # Pregunta: {question}
+    # ---------
+    # Respuesta:
+    """
+
+    PROMPT = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question"]
+    )
+
+    chain = load_qa_chain(OpenAI(), chain_type="stuff") # we are going to stuff all the docs in at once
+
+    chain.llm_chain.prompt = PROMPT
+
+    # # check the prompt
+    # print(chain.llm_chain.prompt.template)
+
+    query = "¿Como puedo configurar el Servidor de deep learning de Cortex? Me podrias dar un ejemplo?"
+    docs = docsearch.similarity_search(query)
+    print(chain.run(input_documents=docs, question=query))
+    
 if __name__ == '__main__':
     main()
